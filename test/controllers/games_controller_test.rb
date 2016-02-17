e61f45172c20f07f5a1f88afc388d23c598871e9
Sandra Harrasser <design@sandraharrasser.com>
2016-02-17 03:05:53 +0000

    added css for chessboard and test for games controller

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  GamesControllerTest
+  test "should get new" do
+  get :new
+  assert_response :success
+  assert_template 'games/new'
+  end
+   
+  test "chess board should be wrapped by one parent div" do
+  get :new
+  assert_select 'div#chessboard', count: 1
+  end
+   
+  test "chess board should contain 8 rows of spaces" do
+  get :new
+  assert_select 'div.board-row', count: 8
+  end
+   
+  test "chess board should contain 64 individual spaces" do
+  get :new
+  assert_select 'div.chessboard-space', count: 64
+  end
+   
+  test "each chess board space should be associated with a coordinate" do
+  get :new
+   
+  ('a'..'h').each do |letter|
+  (1..8).each do |number|
+  assert_select "div##{letter}#{number}", count: 1
+  end
+  end
+  end
 end
