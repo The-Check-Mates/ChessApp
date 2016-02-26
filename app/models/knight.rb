2200b8881738ef6d633ad8efcc04f15ebe245fab
nsiderwolverine <justin.joseph0@yahoo.com>
2016-02-26 21:47:35 +0000

    Add comment to valid_move? for Knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,7 @@
 class Knight < Piece
   
   def valid_move?(x, y)
+  # for eight possible L-shape moves
     ( (x_coordinate - x).abs == 2 && (y_coordinate - y).abs == 1 ) ||
     ( (y_coordinate - y).abs == 2 && (x_coordinate - x).abs == 1 ) && !is_obstructed?
   end
