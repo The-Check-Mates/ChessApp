fe1a0be9d64eb4e0850492d26c68a83518979377
nsiderwolverine <justin.joseph0@yahoo.com>
2016-02-26 21:45:16 +0000

    Add valid move check to Knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,8 @@
 class Knight < Piece
+  
+  def valid_move?(x, y)
+    ( (x_coordinate - x).abs == 2 && (y_coordinate - y).abs == 1 ) ||
+    ( (y_coordinate - y).abs == 2 && (x_coordinate - x).abs == 1 ) && !is_obstructed?
+  end
+  
 end
